e27d595dce859ab5e9de3164d4ac58a7505cacf6
Pablo Lires <pablo@librecomunicacion.net>
2025-01-28 22:32:47 -0300

    agregado de aplicacion react a carpeta landing

---
+++ landing/eslint.config.js
@@ -0,0 +1,24 @@
+import globals from 'globals'
+import pluginJs from '@eslint/js'
+import pluginReact from 'eslint-plugin-react'
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  { files: ['**/*.{js,mjs,cjs,jsx}'] },
+  { languageOptions: { globals: globals.browser } },
+  { ignores: ['**/temp.js', 'config/*', 'dist', 'vendor'] },
+  {
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
+  },
+  {
+    rules: {
+      'react/react-in-jsx-scope': 'off',
+    },
+  },
+  pluginJs.configs.recommended,
+  pluginReact.configs.flat.recommended,
+]
---
+++ landing/src/App.jsx
@@ -0,0 +1,13 @@
+import { React } from 'react'
+
+function App() {
+  return (
+    <>
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quae ullam
+      maxime dolore eveniet cupiditate fuga, ducimus illo, ipsa repellat
+      obcaecati magni? Labore dolore rem eos, quasi reprehenderit enim est
+    </>
+  )
+}
+
+export default App
