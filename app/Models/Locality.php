e9a45b1cc978ad195e80aeea1af6ab33ae4ae5c6
Pablo Lires <pablo@librecomunicacion.net>
2025-02-21 19:43:03 -0300

    modificacion en Model Locality

--- app/Models/Locality.php
+++ app/Models/Locality.php
@@ -2,17 +2,22 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
+use Carbon\Carbon;
 use App\Models\User;
-use App\Models\Partner;
 use App\Models\Zone;
+use App\Models\Partner;
 use App\Models\Province;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Locality extends Model
 {
     /** @use HasFactory<\Database\Factories\LocalityFactory> */
     use HasFactory;
+    use SoftDeletes;
+
+    protected $fillable = ['name', 'zone_id', 'province_id', 'user_id'];
 
     /**
      * Relación muchos a uno: Una localidad pertenece a un socio.
@@ -46,5 +51,15 @@ class Locality extends Model
     public function formSubmissions()
     {
         return $this->hasMany(FormSubmission::class);
+    }
+
+    public function getFormattedModifiedDateAttribute()
+    {
+        return Carbon::parse($this->attributes['updated_at'])->format('d/m/y');
+    }
+
+    public function getFormattedDeletedDateAttribute()
+    {
+        return Carbon::parse($this->attributes['deleted_at'])->format('d/m/y');
     }
 }
