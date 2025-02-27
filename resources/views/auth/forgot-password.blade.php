10224c7ded82a071f4465a497777db533be73dfa
Pablo Lires <pablolires@gmail.com>
2025-02-27 18:34:51 -0300

    Update forgot-password.blade.php

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -1,25 +1,47 @@
 <x-guest-layout>
-    <div class="mb-4 text-sm text-gray-600">
-        {{ __('Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.') }}
-    </div>
 
     <!-- Session Status -->
-    <x-auth-session-status class="mb-4" :status="session('status')" />
+    <div class="lockscreen-wrapper">
+        <x-auth-session-status class="mb-4" :status="session('status')" />
+        <div class="lockscreen-logo">
+            <a href="../../index2.html"><b>PPA</b>RED</a>
+        </div>
+        <!-- User name -->
+        <div class="lockscreen-name">usuario</div>
 
-    <form method="POST" action="{{ route('password.email') }}">
-        @csrf
+        <!-- START LOCK SCREEN ITEM -->
+        <div class="lockscreen-item">
+            <!-- lockscreen image -->
+            <div class="lockscreen-image">
+                <img src="{{ Vite::asset('resources/images/user1-128x128.jpg') }}" alt="User Image">
+            </div>
+            <!-- /.lockscreen-image -->
 
-        <!-- Email Address -->
-        <div>
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
+            <!-- lockscreen credentials (contains the form) -->
+            <form method="POST" action="{{ route('password.email') }}" class="lockscreen-credentials">
+                @csrf
+                <div class="input-group">
+                    <input id="email" name="email" value="{{ old('email') }}" class="form-control"
+                        placeholder="Email" required autofocus>
+
+                    <div class="input-group-append">
+                        <button type="submit" class="btn">
+                            <i class="fas fa-arrow-right text-muted"></i>
+                        </button>
+                    </div>
+                </div>
+            </form>
+            <!-- /.lockscreen credentials -->
+
+        </div>
+        <x-input-error :messages="$errors->get('email')" class="mt-2" />
+        <!-- /.lockscreen-item -->
+        <div class="help-block mb-4 text-sm text-center">
+            {{ __('Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.') }}
+        </div>
+        <div class="text-center">
+            <a href="{{ route('login') }}">O inicie sesión como un usuario diferente</a>
         </div>
 
-        <div class="flex items-center justify-end mt-4">
-            <x-primary-button>
-                {{ __('Email Password Reset Link') }}
-            </x-primary-button>
-        </div>
-    </form>
+    </div>
 </x-guest-layout>
