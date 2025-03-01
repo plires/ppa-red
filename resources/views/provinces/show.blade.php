49f24a5fbc013a760c481959eab1ee3568c80863
Pablo Lires <pablo@librecomunicacion.net>
2025-03-01 15:25:04 -0300

    Update provinces/show.blade.php. Mostrar en dos tablas las zonas y localidades

--- resources/views/provinces/show.blade.php
+++ resources/views/provinces/show.blade.php
@@ -60,21 +60,81 @@
                                     <!-- /.card-footer -->
                                 </div>
                                 <!-- /.card -->
+
                                 <div class="card">
                                     <div class="card-header">
                                         <h3 class="card-title">
                                             <i class="fa-solid fa-location-dot"></i>
-                                            Zonas y localidades asignadas a {{ $province->name }}
+                                            Zonas asignadas a {{ $province->name }}
                                         </h3>
                                     </div>
                                     <!-- /.card-header -->
-                                    @if ($province->zones->isEmpty() && $province->localities->isEmpty())
+                                    @if ($province->zones->isEmpty())
                                         <div class="card-body">
-                                            <p>Esta provincia, no tiene zonas ni localidades</p>
+                                            <p>Esta provincia, no tiene zonas.</p>
                                         </div>
                                     @else
                                         <!-- /.card-header -->
-                                        <div class="card-body table-responsive p-0" style="height: 300px;">
+                                        <div class="card-body table-responsive">
+
+                                            <table id="tableZones"
+                                                class="table table-hover table-head-fixed text-nowrap table-bordered table-striped">
+                                                <thead>
+                                                    <tr>
+                                                        <th>Zona</th>
+                                                        <th>Acción</th>
+                                                    </tr>
+                                                </thead>
+                                                <tbody>
+                                                    @foreach ($province->zones as $zone)
+                                                        <tr>
+                                                            <td>
+                                                                @if (isset($zone->name))
+                                                                    <a title="Ver zona {{ $zone->name }}"
+                                                                        href="{{ route('zones.show', $zone->id) }}">
+                                                                        {{ $zone->name }}
+                                                                    </a>
+                                                                @else
+                                                                    -
+                                                                @endif
+                                                            </td>
+                                                            <td>
+                                                                <a href="{{ route('zones.edit', $zone->id) }}"
+                                                                    type="button" class="btn btn-primary btn-sm">
+                                                                    Editar
+                                                                </a>
+                                                            </td>
+                                                        </tr>
+                                                    @endforeach
+                                                </tbody>
+                                                <tfoot>
+                                                    <tr>
+                                                        <th>Zona</th>
+                                                        <th>Acción</th>
+                                                    </tr>
+                                                </tfoot>
+                                            </table>
+                                        </div>
+                                    @endif
+                                    <!-- /.card-body -->
+                                </div>
+                                <!-- /.card -->
+
+                                <div class="card">
+                                    <div class="card-header">
+                                        <h3 class="card-title">
+                                            <i class="fa-solid fa-location-dot"></i>
+                                            Localidades asignadas a {{ $province->name }}
+                                        </h3>
+                                    </div>
+                                    <!-- /.card-header -->
+                                    @if ($province->localities->isEmpty())
+                                        <div class="card-body">
+                                            <p>Esta provincia, no tiene localidades asignadas</p>
+                                        </div>
+                                    @else
+                                        <!-- /.card-header -->
+                                        <div class="card-body table-responsive">
 
                                             <table id="tableProvinces"
                                                 class="table table-hover table-head-fixed text-nowrap table-bordered table-striped">
@@ -155,6 +215,17 @@
         @vite(['resources/js/jquery.dataTables.min.js', 'resources/js/dataTables.bootstrap4.min.js', 'resources/js/dataTables.responsive.min.js', 'resources/js/responsive.bootstrap4.min.js', 'resources/js/dataTables.buttons.min.js', 'resources/js/buttons.bootstrap4.min.js', 'resources/js/jszip.min.js', 'resources/js/pdfmake.min.js', 'resources/js/vfs_fonts.js', 'resources/js/buttons.html5.min.js', 'resources/js/buttons.print.min.js', 'resources/js/buttons.colVis.min.js', 'resources/js/configure-modal-confirm-delete.js'])
         <script>
             document.addEventListener('DOMContentLoaded', function() {
+                jQuery("#tableZones").DataTable({
+                    "responsive": true,
+                    "lengthChange": false,
+                    "autoWidth": false,
+                    "language": {
+                        url: '/locales/dataTables_es-ES.json',
+                    },
+                    "buttons": ["copy", "csv", "excel", "pdf", "print", "colvis"]
+                }).buttons().container().appendTo(
+                    '#tableProvinces_wrapper .col-md-6:eq(0)');
+
                 jQuery("#tableProvinces").DataTable({
                     "responsive": true,
                     "lengthChange": false,
