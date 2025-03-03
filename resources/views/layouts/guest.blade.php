066dd14f7d16cf0ccf97c72c242ff0fdd9b03d2b
Pablo Lires <pablo@librecomunicacion.net>
2025-03-03 19:10:05 -0300

    Update /layouts/guest.blade.php. Enlace de css y js para vistas publicas

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -6,14 +6,16 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>{{ config('app.name', 'Aplicación PPA RED') }}</title>
 
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
-    <!-- Scripts -->
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    <!-- css y scripts -->
+    @vite(['resources/css/public/app.css', 'resources/js/public/app.js'])
+
+    @yield('css') <!-- Sección específica para CSS extra -->
 </head>
 
 @php
@@ -26,6 +28,8 @@
 
 <body class="{{ $bodyClass }}">
     {{ $slot }}
+
+    @yield('scripts') <!-- Sección específica para JS extra -->
 </body>
 
 </html>
