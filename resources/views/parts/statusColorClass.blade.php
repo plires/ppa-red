c3081034c7d823b9f6dd1d91dcc75fead97e9101
Pablo Lires <pablo@librecomunicacion.net>
2025-03-03 15:53:02 -0300

    Separamos en /parts/statusColorClass.blade.php el archivo para incluir en otras vistas

---
+++ resources/views/parts/statusColorClass.blade.php
@@ -0,0 +1,21 @@
+@php
+    function statusColorClass($status)
+    {
+        switch ($status) {
+            case App\Models\FormSubmissionStatus::STATUS_PENDIENTE_RTA_DE_PARTNER:
+                return 'bg-primary';
+            case App\Models\FormSubmissionStatus::STATUS_RESPONDIO_PARTNER:
+                return 'bg-success';
+            case App\Models\FormSubmissionStatus::STATUS_DEMORADO_POR_PARTNER:
+                return 'bg-danger';
+            case App\Models\FormSubmissionStatus::STATUS_CERRADO_SIN_RTA_PARTNER:
+                return 'bg-warning';
+            case App\Models\FormSubmissionStatus::STATUS_CERRADO_SIN_RTA_USUARIO:
+                return 'bg-secondary';
+            case App\Models\FormSubmissionStatus::STATUS_CERRADO_POR_PARTNER:
+                return 'bg-dark';
+            default:
+                return 'bg-primary'; // Estado por defecto
+        }
+    }
+@endphp
