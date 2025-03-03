45c28124839050563fdf8222a061ed864d408d5f
Pablo Lires <pablo@librecomunicacion.net>
2025-03-03 19:08:57 -0300

    Creacion de estilos y scripts para vistas publicas /public/form_submission/

---
+++ resources/js/public/app.js
@@ -0,0 +1,19 @@
+import $ from 'jquery' // Importa jQuery
+window.$ = window.jQuery = $ // Asigna jQuery a la variable global `$` y `jQuery`
+
+import './../bootstrap'
+
+// Font Awesome
+import '@fortawesome/fontawesome-free/css/all.min.css'
+
+// Theme style
+import 'admin-lte/dist/css/adminlte.min.css'
+
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
+import 'admin-lte/dist/js/adminlte.min.js'
+
+import Alpine from 'alpinejs'
+
+window.Alpine = Alpine
+
+Alpine.start()
