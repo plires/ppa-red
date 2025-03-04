a593ceb29879f9bf021f89e4986dbed17a4b0563
Pablo Lires <pablo@librecomunicacion.net>
2025-03-04 14:29:11 -0300

    Update /PublicFormResponseController.php. Actualiza el estado del FormSubmission luego de guardar el mensage.

--- app/Http/Controllers/PublicFormResponseController.php
+++ app/Http/Controllers/PublicFormResponseController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\FormResponse;
+use App\Models\FormSubmission;
 use Illuminate\Support\Facades\Mail;
 use App\Mail\FormResponseMailToPartner;
 use App\Http\Requests\PublicFormResponseRequest;
@@ -19,6 +20,11 @@ class PublicFormResponseController extends Controller
         // Enviar el email
         Mail::to($formResponse->user->email)
             ->send(new FormResponseMailToPartner($formResponse, $formSubmission));
+
+        // Actualizar el estado del FormSubmission
+        $formSubmission = FormSubmission::findOrFail($request['form_submission_id']);
+        $formSubmission->form_submission_status_id = 1;
+        $formSubmission->save();
 
         return back()->with('success', 'El mensaje se envió correctamente. En breve será respondido.');
     }
