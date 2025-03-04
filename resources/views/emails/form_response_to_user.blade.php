e8bc6337f57d30c95e6157cc4bf9c5fa9420be9a
Pablo Lires <pablo@librecomunicacion.net>
2025-03-03 23:46:52 -0300

    Create vistas email. Para nuevos comentarios de usuarios y Partners

---
+++ resources/views/emails/form_response_to_user.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Nuevo Mensaje</title>
+</head>
+
+<body>
+    <h2>Mensaje para el usuario {{ $data['name'] }}</h2>
+    <p><strong>Mensaje:</strong> {{ $formResponse->message }}</p>
+    <p><strong>Enviado por:</strong> {{ $formResponse->user->name ?? 'Usuario anónimo' }}</p>
+    <p>Fecha: {{ $formResponse->created_at->format('d/m/Y H:i') }}</p>
+</body>
+
+</html>
