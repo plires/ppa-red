4d8b1e2212da47adcbd8cbea3b53eb25976056bc
Pablo Lires <pablo@librecomunicacion.net>
2025-03-03 23:51:29 -0300

    Update /FormResponseController.php. Envio de mail en el metodo store.

--- app/Http/Controllers/FormResponseController.php
+++ app/Http/Controllers/FormResponseController.php
@@ -2,11 +2,9 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\User;
 use App\Models\FormResponse;
-use Illuminate\Http\Request;
-use App\Models\FormSubmission;
-use Illuminate\Support\Facades\Auth;
+use App\Mail\FormResponseMailToUser;
+use Illuminate\Support\Facades\Mail;
 use App\Http\Requests\FormResponseRequest;
 
 class FormResponseController extends Controller
@@ -15,7 +13,12 @@ class FormResponseController extends Controller
     public function store(FormResponseRequest $request)
     {
 
-        FormResponse::create($request->validated());
+        $formResponse = FormResponse::create($request->validated());
+        $data = json_decode($formResponse->formSubmission->data, true); // Convierte JSON en array
+
+        // Enviar el email
+        Mail::to($data['email'])
+            ->send(new FormResponseMailToUser($formResponse, $data));
 
         return back()->with('success', 'El mensaje se envió correctamente.');
     }
