1c66f98f8b8b49d585434dc5c8623451572e1894
Pablo Lires <pablo@librecomunicacion.net>
2025-03-11 22:47:20 -0300

    Update /views/parts/modal-closure-reason.blade.php

--- resources/views/parts/modal-closure-reason.blade.php
+++ resources/views/parts/modal-closure-reason.blade.php
@@ -3,28 +3,28 @@
     <div class="modal-dialog" role="document">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title" id="closureReasonLabel">Confirmar Envío</h5>
+                <h5 class="modal-title" id="closureReasonLabel">Razón de cierre</h5>
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
                 </button>
             </div>
-            <div class="modal-body">
-                <form id="closure-reason" class="form-horizontal" method="POST"
-                    action="{{ route('form_submissions.update', $formSubmission->id) }}">
-                    @csrf
-                    @method('PUT')
+            <form id="closure-reason" class="form-horizontal" method="POST"
+                action="{{ route('form_submissions.update', $formSubmission->id) }}">
+                @csrf
+                @method('PUT')
+                <div class="modal-body">
 
                     <input type="hidden" name="form_submission_id" value="{{ $formSubmission->id }}">
                     <input type="hidden" name="user_id" value="{{ $user->id }}">
-                    <input type="text" name="closure_reason" value="{{ old('closure_reason') }}">
+                    <textarea required class="form-control" name="closure_reason" rows="3"
+                        placeholder="Escriba la razón de este cierre... (obligatorio) (no será visible por el usuario)">{{ old('closure_reason') }}</textarea>
 
-                </form>
-            </div>
-            <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancelar</button>
-                <!-- Botón para enviar el mensaje después de confirmar -->
-                <button type="submit" class="btn btn-primary" form="message-form">Cerrar Formulario</button>
-            </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancelar</button>
+                    <button type="submit" class="btn btn-primary" form="closure-reason">Cerrar Formulario</button>
+                </div>
+            </form>
         </div>
     </div>
 </div>
