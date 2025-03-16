60ada270f791365c94f5defe9da95d8759a029e3
Pablo Lires <pablo@librecomunicacion.net>
2025-03-16 14:29:34 -0300

    Edit config/form_submission_closure_reasons.php. Cambio en leyenda

--- config/form_submission_closure_reasons.php
+++ config/form_submission_closure_reasons.php
@@ -5,7 +5,7 @@ return [
     'closed_no_reply_never_user' => 'Esta consulta estuvo inactiva durante 7 días. El usuario sólo envió el mensaje inicial.',
     'closed_no_reply_user' => 'Esta consulta estuvo inactiva durante 7 días sin más interacción por parte del usuario.',
     'closed_no_reply_never_partner' => 'Esta consulta estuvo inactiva durante 7 días',
-    'closed_no_reply_partner' => 'Esta consulta tuvo interacción entre el partner y el usuario, pero estuvo inactiva durante 7 días',
+    'closed_no_reply_partner' => 'Cerrado automáticamente en consultas con estado "demorado", tras 7 días de inactividad',
     'closed_by_partner' => 'El partner ha cerrado esta consulta de manera manual.',
   ]
 ];
