4ecf5c06c321a6b900194e2b88d85e6c2eb45bfc
Pablo Lires <pablo@librecomunicacion.net>
2025-03-26 22:40:18 -0300

    Update /Models/FormResponse.php. Agregado de metodos en modelo FormResponse.

--- app/Models/FormResponse.php
+++ app/Models/FormResponse.php
@@ -24,4 +24,20 @@ class FormResponse extends Model
     {
         return $this->belongsTo(User::class);
     }
+
+    // Método para marcar la notificación como leída
+    public function markAsRead(): void
+    {
+        $this->is_read = true;
+        $this->read_at = now();
+        $this->save();
+    }
+
+    // Método para marcar la notificación como no leída
+    public function markAsUnread(): void
+    {
+        $this->is_read = false;
+        $this->read_at = null;
+        $this->save();
+    }
 }
