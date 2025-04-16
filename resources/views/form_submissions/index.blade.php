397fda21bbce7e26f8aca172fbbf1769c1718b26
Pablo Lires <pablo@librecomunicacion.net>
2025-04-16 19:07:52 -0300

    Update /form_submissions/index.blade.php. Agregado de botones de exportacion al DataTable

--- resources/views/form_submissions/index.blade.php
+++ resources/views/form_submissions/index.blade.php
@@ -153,16 +153,19 @@
         @vite(['resources/js/jquery.dataTables.min.js', 'resources/js/dataTables.bootstrap4.min.js', 'resources/js/dataTables.responsive.min.js', 'resources/js/responsive.bootstrap4.min.js', 'resources/js/dataTables.buttons.min.js', 'resources/js/buttons.bootstrap4.min.js', 'resources/js/jszip.min.js', 'resources/js/pdfmake.min.js', 'resources/js/vfs_fonts.js', 'resources/js/buttons.html5.min.js', 'resources/js/buttons.print.min.js', 'resources/js/buttons.colVis.min.js'])
         <script>
             document.addEventListener('DOMContentLoaded', function() {
-                jQuery("#tableFormSubmissions").DataTable({
-                    "responsive": true,
-                    "lengthChange": false,
-                    "autoWidth": false,
-                    "language": {
+                jQuery('#tableFormSubmissions').DataTable({
+                    responsive: true,
+                    lengthChange: false,
+                    autoWidth: false,
+                    language: {
                         url: '/locales/dataTables_es-ES.json',
                     },
-                    "buttons": ["copy", "csv", "excel", "pdf", "print", "colvis"]
-                }).buttons().container().appendTo(
-                    '#tableFormSubmissions_wrapper .col-md-6:eq(0)');
+                    buttons: ["excel", "pdf", "print", "colvis"],
+                    initComplete: function() {
+                        this.api().buttons().container()
+                            .appendTo('#tableFormSubmissions_wrapper .col-md-6:eq(0)');
+                    }
+                });
             });
         </script>
     @endsection
