2fa2f5976bde61cc8374b8ba8b5de264dbc99fad
Pablo Lires <pablo@librecomunicacion.net>
2025-04-18 17:57:40 -0300

    Update. Adecuacion de layout y estilos en vista reset-password

--- resources/views/auth/reset-password.blade.php
+++ resources/views/auth/reset-password.blade.php
@@ -1,39 +1,77 @@
 <x-guest-layout>
-    <form method="POST" action="{{ route('password.store') }}">
-        @csrf
 
-        <!-- Password Reset Token -->
-        <input type="hidden" name="token" value="{{ $request->route('token') }}">
+    @section('css')
+        <!-- DataTables -->
+        @vite(['resources/css/auth/reset-password.css'])
+    @endsection
 
-        <!-- Email Address -->
-        <div>
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email', $request->email)" required autofocus autocomplete="username" />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
+    <section class="login-page resetPassword">
+        <div class="login-box">
+
+            <div class="card card-outline card-primary">
+                <div class="card-header text-center">
+                    <p class="h1"><b>PPA </b>RED</p>
+                </div>
+                <div class="card-body">
+                    <p class="login-box-msg">Login para iniciar tu sesión</p>
+
+                    <form method="POST" action="{{ route('password.store') }}">
+                        @csrf
+
+                        <!-- Password Reset Token -->
+                        <input type="hidden" name="token" value="{{ $request->route('token') }}">
+
+                        <!-- Email Address -->
+                        <x-input-label for="email" :value="__('Email')" />
+                        <div class="input-group mb-3">
+                            <input id="email" type="email" name="email"
+                                value="{{ old('email', $request->email) }}" class="form-control" placeholder="Email"
+                                required autofocus autocomplete="username">
+                            <div class="input-group-append">
+                                <div class="input-group-text">
+                                    <span class="fas fa-envelope"></span>
+                                </div>
+                            </div>
+                        </div>
+                        <x-input-error :messages="$errors->get('email')" class="mt-2" />
+
+                        <!-- Password -->
+                        <x-input-label for="password" :value="__('Password')" />
+                        <div class="input-group mb-3">
+                            <input id="password" type="password" name="password" class="form-control"
+                                placeholder="Password" required autocomplete="new-password">
+                            <div class="input-group-append">
+                                <div class="input-group-text">
+                                    <span class="fas fa-lock"></span>
+                                </div>
+                            </div>
+                        </div>
+                        <x-input-error :messages="$errors->get('password')" class="mt-2" />
+
+                        <!-- Confirm Password -->
+                        <x-input-label for="password_confirmation" :value="__('Confirm Password')" />
+                        <div class="input-group mb-3">
+                            <input id="password_confirmation" type="password" name="password_confirmation"
+                                class="form-control" placeholder="Password" required autocomplete="new-password">
+                            <div class="input-group-append">
+                                <div class="input-group-text">
+                                    <span class="fas fa-lock"></span>
+                                </div>
+                            </div>
+                        </div>
+                        <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
+
+                        <div class="row">
+                            <div class="col-12 text-center">
+                                <button type="submit" class="btn btn-primary btn-block">Restablecer Contraseña</button>
+                            </div>
+                        </div>
+
+                    </form>
+
+                </div>
+                <!-- /.card-body -->
+            </div>
         </div>
-
-        <!-- Password -->
-        <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')" />
-            <x-text-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="new-password" />
-            <x-input-error :messages="$errors->get('password')" class="mt-2" />
-        </div>
-
-        <!-- Confirm Password -->
-        <div class="mt-4">
-            <x-input-label for="password_confirmation" :value="__('Confirm Password')" />
-
-            <x-text-input id="password_confirmation" class="block mt-1 w-full"
-                                type="password"
-                                name="password_confirmation" required autocomplete="new-password" />
-
-            <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
-        </div>
-
-        <div class="flex items-center justify-end mt-4">
-            <x-primary-button>
-                {{ __('Reset Password') }}
-            </x-primary-button>
-        </div>
-    </form>
+    </section>
 </x-guest-layout>
