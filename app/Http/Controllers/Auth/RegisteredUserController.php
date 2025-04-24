1a06eb2619dc3105ac6d8b37d2ab37b292591b5c
Pablo Lires <pablo@librecomunicacion.net>
2025-04-23 23:20:54 -0300

    Update /Controllers/Auth/RegisteredUserController.php. Cambio de comportamiento al registrar nuevo partner

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -47,8 +47,8 @@ class RegisteredUserController extends Controller
 
         event(new Registered($user));
 
-        Auth::login($user);
+        // Auth::login($user);
 
-        return redirect()->intended('/form_submissions');
+        return redirect()->intended('/partners');
     }
 }
